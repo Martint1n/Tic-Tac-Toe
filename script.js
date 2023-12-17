35ae40d7fa4ce6eaa693e4047a53eac325e08116
Martint1n <149445595+Martint1n@users.noreply.github.com>
2023-12-17 11:53:13 +0100

    Adding rules with error : undefined is not an obj

--- script.js
+++ script.js
@@ -1,42 +1,80 @@
-/* player 1 {mark X}
-player 2 {mark O}
-
-addeventlistener click 
-    if nombre mark X > nombre mark O 
-        use mark O 
-    else 
-        use mark X
-*/
 
 let game = gameBoard();
-/*let count = {};
-for (let i in game){
-    for (const element of game[i]) {
-        count[element] = (count[element] || 0) + 1;
-    }
-    }
-console.log(game)
-console.log(count)*/
 
-function selectMarker(nombre1, nombre2, board){
-    let count = {};
-    for (let i in game){
-        for (const element of board[i]) {
-        count[element] = (count[element] || 0) + 1;
+const player = {
+    player1: "X",
+    player2: "O",
+    selectMarker(nombre1, nombre2, board, button){
+        let count = {};
+        for (let i in game){
+            for (const element of board[i]) {
+            count[element] = (count[element] || 0) + 1;
+            }
         }
+            if (count[""] %2 !== 0){
+                board[nombre1][nombre2] = this.player1;
+                button.innerHTML = `${this.player1}`
+            }else{
+                board[nombre1][nombre2] = this.player2;
+                button.innerHTML = `${this.player2}`
+            }
+        game = board;
+        console.log(game);
+        console.log(count[""])
+    },
+    rule(){
+        for (let row = 0; row < game.length; row++){
+            if (game[row][0] == game[row][1] && game[row][1] == game[row][2] && game[row][2] !== ""){
+                if (game[row][0] == "X"){
+                    window.alert("winner");
+                    this.reset();
+                }else {
+                    window.alert("winner O");
+                    this.reset();
+                }
+            }else if (game[0][row] == game[1][row] && game[1][row] == game[2][row] && game[2][row] !== ""){
+                if (game[row][0] == "X"){
+                    window.alert("winner");
+                    this.reset();
+                }else {
+                    window.alert("winner O");
+                    this.reset();
+                }
+            }else if (game[row][row] == game[row+1][row+1] && game[row+1][row+1] == game[row+2][row+2] && game[row+2][row+2] !== ""){
+                if (game[row][row] == "X"){
+                    window.alert("winner");
+                    this.reset();
+                }else {
+                    window.alert("winner O");
+                    this.reset();
+                }
+            }else if (game[row][row+2] == game[row+1][row+1] && game[row+1][row+1] == game[row+2][row] && game[row+2][row] !== ""){
+                if (game[row][row+2] == "X"){
+                    window.alert("winner");
+                    this.reset();
+                }else {
+                    window.alert("winner O");
+                    this.reset();
+                }
+            }
+        }
+    },
+    reset(){
+        game = gameBoard();
+        buttons.forEach(function(button){
+            button.innerHTML = "" ;
+        })
     }
-        if (count[""] %2 !== 0){
-            board[nombre1][nombre2] = "X";
-        }else{
-            board[nombre1][nombre2] = "O";
-        }
-    game = board;
-    rule();
-    console.log(game);
-    console.log(count[""])
 }
 
-function rule(){
+const buttons = document.querySelectorAll(".test")
+buttons.forEach(function(button) {
+    button.addEventListener("click", () => {
+        player.selectMarker(button.getAttribute("data-row"),button.getAttribute("data-column"),game, button);
+        player.rule();
+    })
+});
+
     /* conditions de victoire lignes
     [0][0],[0][1],[0][2] ; [1][0],[1][1],[1][2] ; [2][0],[2][1],[2][2]
 
@@ -45,18 +83,71 @@ function rule(){
 
     conditions de victoire diagonales
     [0][0],[1][1],[2][2] ; [0][2],[1][1],[2][0]
-    */
-    if (game[0][0] == "X" && game[0][1] == "X" && game[0][2] == "X"){
-        console.log("win")
-    }else if (game[1][0] == game[1][1] == game[1][2] == "X" || "O"){
 
+    const winningCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // colums
+    [0, 4, 8],
+    [2, 4, 6]
+    ]
+
+    const winningCombo = [
+        [[0,0],[0,1],[0,2]],
+        [[1,0],[1,1],[1,2]],
+        [[2,0],[2,1],[2,2]], // rows
+        [[0,0],[1,0],[2,0]],
+        [[0,1],[1,1],[2,1]],
+        [[0,2],[1,2],[2,2]], // columns
+        [[0,0],[1,1],[2,2]],
+        [[0,2],[1,1],[2,0]],
+    ]*/
+
+    /*const winningComboDeux = {
+        win1: [[[0][0]],[[0][1]],[[0][2]]],
+        win2: [[[1][0]],[[1][1]],[[1][2]]],
+        win3: [[[2][0]],[[2][1]],[[2][2]]], // rows
+        win4: [[[0][0]],[[1][0]],[[2][0]]],
+        win5: [[[0][1]],[[1][1]],[[2][1]]],
+        win6: [[[0][2]],[[1][2]],[[2][2]]], // columns
+        win7: [[[0][0]],[[1][1]],[[2][2]]],
+        win8: [[[0][2]],[[1][1]],[[2][0]]], 
     }
-    /*
+
+
+
+    const testWin = [position]
+    si les positions = entre elles et différeentes de "", alors win 
+
+    if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && game[0][2] == player1.marker ){
+        console.log(player1.name, "win");
+    }else if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && game[0][2] == player2.marker ){
+        console.log(player2.name, "win");
+
+
+    }else if (game[1][0] ==  game[1][1] && game[1][1] == game[1][2] && game[1][2] == player1.marker){
+        console.log("win");
+    }else if (game[1][0] ==  game[1][1] && game[1][1] == game[1][2] && game[1][2] == player2.marker){
+        console.log("win");
+
+    }else if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && game[2][2] == player1.marker){
+        console.log("win");
+    }else if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && game[2][2] == player2.marker){
+        console.log("win");
+    
+    }else if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && game[2][0] == player1.marker){
+        console.log("win");
+    }else if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && game[2][0] == player2.marker){
+        console.log("win");
+    
+    }
     i, j et k  = []
     i, j et k dans game pour concate 
     si i 
     */
-}
 
 function gameBoard(){
     const rows = 3
@@ -130,18 +221,6 @@ console.log(count);
 // 👇️ {one: 3, two: 2, three: 1}
 console.log(count);*/
 
-function createPlayer(name, marker){
-    return {name, marker}
-}
-const player1 = createPlayer("player1", "X");
-const player2 = createPlayer("Player2", "O");
-
-function activePlayer(){
-    //while //board[] "" even => player1 
-    //else player2
-}
-
-
 
 /*function createPlayer(name, marker){
     this.name = name
@@ -163,3 +242,28 @@ const player2 = new createPlayer("Playerr2", "O")*/
         }
     }
 }*/
+
+/*
+function selectMarkerTest(nombre1, nombre2, board){
+    let count = {};
+    for (let i in game){
+        for (const element of board[i]) {
+        count[element] = (count[element] || 0) + 1;
+        }
+    }
+        if (count[""] %2 !== 0){
+            board[nombre1][nombre2] = player1.marker;
+        }else{
+            board[nombre1][nombre2] = player2.marker;
+        }
+    game = board;
+    rule();
+    console.log(game);
+    console.log(count[""])
+}
+
+const btn = document.querySelectorAll("tic")
+
+btn.addEventListener("click", () => {
+
+})*/
